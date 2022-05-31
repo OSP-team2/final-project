9bc4daa3175b28f09b681100d7865c317901f3e9
Mjunghyun <mjh2061@naver.com>
2022-05-31 11:29:45 +0000

    add grade select form feature

--- app.py
+++ app.py
@@ -1,51 +1,59 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import SelectField, SubmitField
 from pymongo import MongoClient
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+class InfoForm(FlaskForm):
+    grade = SelectField('연도 선택:', choices=[('grade_21', '2021학년도'), ('grade_20', '2020학년도'), ('grade_19', '2019학년도')])
+    submit = SubmitField('Submit')
 
 client = MongoClient()
 db = client.it_college
 dept = db.dept
 
-img_path = "../static/images/"
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/electronics')
+@app.route('/electronics', methods=['GET', 'POST'])
 def electronics():
+    form = InfoForm()
+    path = "grade_21"
+    
+    if form.validate_on_submit():
+        path = form.grade.data
+    
     data = dept.find_one({ 'name': "school of electronics engineering" })
-    after_graduation = img_path + data['images']['after_graduation']
     
-    grade_21 = img_path + data['images']['grade_21']
-    grade_20 = img_path + data['images']['grade_20']
-    grade_19 = img_path + data['images']['grade_19']
-    
-    return render_template('electronics.html', data=data, after_graduation=after_graduation, grade_21=grade_21,
-                           grade_20=grade_20, grade_19=grade_19)
+    return render_template('electronics.html', form=form, path=path, data=data)
 
-@app.route('/computer')
+@app.route('/computer', methods=['GET', 'POST'])
 def computer():
+    form = InfoForm()
+    path = "grade_21"
+    
+    if form.validate_on_submit():
+        path = form.grade.data
+        
     data = dept.find_one({ 'name': "school of computer science and engineering" })
-    after_graduation = img_path + data['images']['after_graduation']
     
-    grade_21 = img_path + data['images']['grade_21']
-    grade_20 = img_path + data['images']['grade_20']
-    grade_19 = img_path + data['images']['grade_19']
-    return render_template('computer.html', data=data, after_graduation=after_graduation, grade_21=grade_21,
-                           grade_20=grade_20, grade_19=grade_19)
+    return render_template('computer.html', form=form, path=path, data=data)
 
-@app.route('/electric')
+@app.route('/electric', methods=['GET', 'POST'])
 def electric():
-    data = dept.find_one({ 'name': "department of electrical engineering" })
-    after_graduation = img_path + data['images']['after_graduation']
+    form = InfoForm()
+    path = "grade_21"
     
-    grade_21 = img_path + data['images']['grade_21']
-    grade_20 = img_path + data['images']['grade_20']
-    grade_19 = img_path + data['images']['grade_19']
-    return render_template('electric.html', data=data, after_graduation=after_graduation, grade_21=grade_21,
-                           grade_20=grade_20, grade_19=grade_19)
+    if form.validate_on_submit():
+        path = form.grade.data
+        
+    data = dept.find_one({ 'name': "department of electrical engineering" })
+    
+    return render_template('electric.html', form=form, path=path, data=data)
 
 @app.route('/mobile')
 def mobile():
