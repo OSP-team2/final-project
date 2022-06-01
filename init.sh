d9a609fa546aa88a7050b1d1623e74a619b81eca
junsoo <1herjs1@gmail.com>
2022-06-01 08:12:51 +0000

    update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo "update & upgrade packages"
+sudo apt -y update && sudo apt -y upgrade
+
+echo "install Python"
+sudo apt install -y python3
+
+echo "install pip"
+sudo apt install -y python3-pip
+
+echo "install MongoDB"
+sudo apt install -y mongodb
+
 echo "create venv"
 python3 -m venv .venv
 
